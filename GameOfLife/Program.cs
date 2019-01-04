763c5f0ea990786fe121c40a628fcb3536cf97ea
tiffie44 <tiffanie.truong@hotmail.com>
2019-01-03 19:11:40 -0500

    This is a test!

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -14,6 +14,7 @@ namespace GameOfLife
         [STAThread]
         static void Main()
         {
+            // hi this is comment #5
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
