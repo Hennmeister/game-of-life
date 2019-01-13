6f6fb05ce3a5885cca370109b1351ccb6a4fb89d
Hennmeister <henning.lindig@rogers.com>
2019-01-12 22:22:15 -0500

    Implemented basic data factory features.

---
+++ GameOfLife/UnitFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class UnitFactory
+    {
+        public UnitFactory() { }
+        
+        public Unit CreateUnit(int UnitTypeIndex)
+        {
+            UnitTypes myType = (UnitTypes)UnitTypeIndex;
+            switch (myType)
+            {
+                case UnitTypes.Cell:
+                    return new Cell();
+                case UnitTypes.Colony:
+                    return new Colony();
+                case UnitTypes.Animal:
+                    return new Animal();
+                case UnitTypes.Plant:
+                    return new Plant();
+                case UnitTypes.Virus:
+                    return new Virus();
+                default:
+                    return null;
+            }
+        }
+        
+    }
+}
