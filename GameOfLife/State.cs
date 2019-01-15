75d07b1358c3eef0b4f931f7b1b056b6372e5688
Rudy Ariaz <rudyal147@gmail.com>
2019-01-15 13:17:52 -0500

    Added environment class.

---
+++ GameOfLife/State.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Rudy Ariaz
+ * January 15th, 2019
+ * Wraps and encapsulates all information about the game state. 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class State
+    {
+        public int CurrentScore { get; set; }
+        public int HighestConcurrentScore { get; set; }
+        public string Username { get; set; }
+        public int GenerationCounter { get; set; }
+        private Environment gameEnvironment;
+
+        public State()
+        {
+            GenerationCounter = 0;
+        }
+
+        public UpdateBlock(Unit unit, int row, int col)
+        {
+
+        }
+
+        public 
+
+
+    }
+}
