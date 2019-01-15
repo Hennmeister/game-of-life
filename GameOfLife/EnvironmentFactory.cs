6230b88bb117144cb460eea07794811ae866f0d0
Rudy Ariaz <rudyal147@gmail.com>
2019-01-15 14:06:36 -0500

    Added complete environment factory.

---
+++ GameOfLife/EnvironmentFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    static class EnvironmentFactory
+    {
+        public Environment CreateEnvironment(Enums.EnvironmentType environmentType)
+        {
+            switch (environmentType)
+            {
+                case Enums.EnvironmentType.Rainforest:
+                    return new Rainforest();
+                case Enums.EnvironmentType.Greenhouse:
+                    return new Greenhouse();
+                case Enums.EnvironmentType.Tundra:
+                    return new Tundra();
+                case Enums.EnvironmentType.Desert:
+                    return new Desert();
+                default:
+                    return null;
+            }
+        }
+    }
+}
