2be85a303239e161292aa88f00efd27ff371378e
tiffie44 <tiffanie.truong@hotmail.com>
2019-01-16 13:29:26 -0500

    Coding Cell methods

--- GameOfLife/Unit.cs
+++ GameOfLife/Unit.cs
@@ -9,6 +9,10 @@ namespace GameOfLife
 {
     public abstract class Unit
     {
+        // Specifies the dimension used for GetLength()
+        public const int ROW = 0;
+        public const int COLUMN = 1;
+
         protected System.Drawing.Color BaselineColor { get; }
         protected double DecompositionValue { get; }
 
