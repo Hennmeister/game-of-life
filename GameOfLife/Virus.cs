7e01f4ad04f4530f70f1405e0238d3a8b263fdd7
Rudy Ariaz <rudyal147@gmail.com>
2019-01-18 12:45:12 -0500

    Added infection stuff

--- GameOfLife/Virus.cs
+++ GameOfLife/Virus.cs
@@ -26,14 +26,23 @@ namespace GameOfLife
             
         }
 
-        private bool Infect(Unit[,] grid, int row, int col)
+        private void Infect(Unit[,] grid, int row, int col)
         {
             foreach(var dir in GridHelper.directions)
             {
                 int newRow = row + dir.Item1;
                 int newCol = col + dir.Item2;
+                Unit neighbour = grid[newRow, newCol];
                 // Check if there is a living unit in the cell
+                // Infects a unit even if it is already infected
+                if(neighbour is LivingUnit && neighbour != null)
+                {
+                    // Infect 
+                    (neighbour as LivingUnit).BeInfected();
+                    break;
+                }
             }
+
         }
     }
 }
