adb558335c8893185690aed5c607b44b5798d30c
Rudy Ariaz <rudyal147@gmail.com>
2019-01-18 11:58:57 -0500

    Added a GridHelper

--- GameOfLife/Virus.cs
+++ GameOfLife/Virus.cs
@@ -28,7 +28,12 @@ namespace GameOfLife
 
         private bool Infect(Unit[,] grid, int row, int col)
         {
-            throw new NotImplementedException();
+            foreach(var dir in GridHelper.directions)
+            {
+                int newRow = row + dir.Item1;
+                int newCol = col + dir.Item2;
+                // Check if there is a living unit in the cell
+            }
         }
     }
 }
